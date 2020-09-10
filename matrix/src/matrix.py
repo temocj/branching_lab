8e802c9c30aa7754ae414e5bd95e7166dac0d471
temocj <cuauhtemoc.jacobson@gmail.com>
2020-09-10 20:15:06 +0100

    refactored Matrix constructor

--- matrix/src/matrix.py
+++ matrix/src/matrix.py
@@ -6,24 +6,25 @@ class Matrix:
 
         index = 0
         length = len(matrix_string)
-        number = ""
         row = 0
         col = 0
         while index < length:
             char = matrix_string[index]
             if char == " ":
-                self._commit_number(number, row, col)
-                number = ""
                 col += 1
             elif char == "\n":
-                self._commit_number(number, row, col)
-                number = ""
                 row += 1
-                col = 0
+                col  = 0
             else:
-                number = number + char
+                next_space   = matrix_string.find( " ", index)
+                next_newline = matrix_string.find("\n", index)
+                next_space   = next_space   if next_space   != -1 else length
+                next_newline = next_newline if next_newline != -1 else length
+                number_end = min(next_space, next_newline)
+                number = matrix_string[index:number_end]
+                self._commit_number(number, row, col)
+                index += max(0, len(number) - 1)
             index += 1
-        self._commit_number(number, row, col)
 
     def _commit_number(self, number, row, col):
         if number != "":
