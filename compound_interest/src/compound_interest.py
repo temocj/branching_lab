f22d1d5b6ddcdb4212480199542a996d1ba06a0b
temocj <cuauhtemoc.jacobson@gmail.com>
2020-09-10 14:14:08 +0100

    compound_interest MVP

--- compound_interest/src/compound_interest.py
+++ compound_interest/src/compound_interest.py
@@ -1,4 +1,12 @@
 import pdb
 
 class CompoundInterest:
-    pass
+
+    @staticmethod
+    def calculate(starting_amount, interest_rate, years):
+        P = starting_amount
+        r = interest_rate/100.00
+        t = years
+        n = 12.00
+        A = P * ( 1.00 + r/n )**( n * t )
+        return round(A, 2)
--- compound_interest/tests/compound_interest_test.py
+++ compound_interest/tests/compound_interest_test.py
@@ -4,18 +4,35 @@ from src.compound_interest import CompoundInterest
 
 class CompoundInterestTest(unittest.TestCase):
 
+    def setUp(self):
+        pass
+
     # Tests
 
     # Should return 732.81 given 100 principal, 10 percent, 20 years
-
+    def test_a(self):
+        actual = CompoundInterest.calculate(100.00, 10.00, 20)
+        self.assertEqual(732.81, actual)
+    
     # Should return 181.94 given 100 principal, 6 percent, 10 years
+    def test_b(self):
+            actual = CompoundInterest.calculate(100.00, 6.00, 10)
+            self.assertEqual(181.94, actual)
 
     # Should return 149,058.55 given 100000 principal, 5 percent, 8 years
+    def test_c(self):
+        actual = CompoundInterest.calculate(100000.00, 5.00, 8)
+        self.assertEqual(149058.55, actual)
 
     # Should return 0.00 given 0 principal, 10 percent, 1 year
+    def test_zero_principal(self):
+        actual = CompoundInterest.calculate(0.00, 10.00, 1)
+        self.assertEqual(0.00, actual)
 
     # Should return 100.00 given 100 principal, 0 percent, 10 years
-
+    def test_zero_percent(self):
+            actual = CompoundInterest.calculate(100.00, 0.00, 10)
+            self.assertEqual(100.00, actual)
 
     # Extention tests
 
