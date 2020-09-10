ee1bbea64978fe6f698be672d1aa3d699355190f
temocj <cuauhtemoc.jacobson@gmail.com>
2020-09-10 17:02:18 +0100

    implemented Matrix. all tests passing

--- matrix/src/matrix.py
+++ matrix/src/matrix.py
@@ -1,9 +1,45 @@
+import pdb
+
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.data = []
+
+        index = 0
+        length = len(matrix_string)
+        number = ""
+        row = 0
+        col = 0
+        while index < length:
+            char = matrix_string[index]
+            if char == " ":
+                self._commit_number(number, row, col)
+                number = ""
+                col += 1
+            elif char == "\n":
+                self._commit_number(number, row, col)
+                number = ""
+                row += 1
+                col = 0
+            else:
+                number = number + char
+            index += 1
+        self._commit_number(number, row, col)
+
+    def _commit_number(self, number, row, col):
+        if number != "":
+            if row >= len(self.data):
+                self.data.append([int(number)])
+            else:
+                self.data[row].append(int(number))
 
     def row(self, index):
-        pass
+        row = []
+        for el in self.data[index-1]:
+            row.append(el)
+        return row
 
     def column(self, index):
-        pass
+        col = []
+        for i in range(0, len(self.data)):
+            col.append(self.data[i][index-1])
+        return col
--- matrix/tests/matrix_test.py
+++ matrix/tests/matrix_test.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest, pdb
 
 from src.matrix import Matrix
 
@@ -6,7 +6,7 @@ class MatrixTest(unittest.TestCase):
     # Tests
     def setUp(self):
         self.matrix_1 = Matrix("9 8 7\n5 3 2\n6 6 7")        # square matrix
-        self.matrix_2 = Matrix("1 2 3\n4 5 6\n7 8 9\n9 7 6") # tall matrix
+        self.matrix_2 = Matrix("1 2 3\n4 5 6\n7 8 9\n8 7 6") # tall matrix
         self.matrix_3 = Matrix("1 2 3 4\n5 6 8 8\n9 8 7 6")  # wide matrix
         self.matrix_4 = Matrix("89 1903 3\n18 3 1\n9 4 800") # irregular matrix
 
@@ -16,7 +16,7 @@ class MatrixTest(unittest.TestCase):
 
     # Test can extract a given row
     def test_extract_row(self):
-        self.assertEqual([9, 8, 7], self.matrix_1.row(0))
+        self.assertEqual([9, 8, 7], self.matrix_1.row(1))
 
     # Test can extract a given row where numbers have different number of digits
     # Example matrix:
@@ -24,7 +24,7 @@ class MatrixTest(unittest.TestCase):
     # 1 2
     # 10 20
     def test_extract_row_variable_digits(self):
-        self.assertEqual([1, 2], Matrix("1 2\n10 20").row(0))
+        self.assertEqual([1, 2], Matrix("1 2\n10 20").row(1))
 
     # Test can extract row from non-square matrix
     # Example matrix:
@@ -34,15 +34,15 @@ class MatrixTest(unittest.TestCase):
     # 7 8 9
     # 8 7 6
     def test_extract_row__non_square_matrix(self):
-        self.assertEqual([8, 7, 6], self.matrix_2.row(3))
+        self.assertEqual([8, 7, 6], self.matrix_2.row(4))
 
     # Test can extract a column
     def test_extract_column(self):
-        self.assertEqual([9, 5, 6], self.matrix_1.column(0))
+        self.assertEqual([9, 5, 6], self.matrix_1.column(1))
 
     # Test can extract column from a one number matrix
     def test_extract_column_from_one_number_matrix(self):
-        self.assertEqual([4], Matrix("4"))
+        self.assertEqual([4], Matrix("4").column(1))
 
     # Test can extract a column from non-square matrix
     # Example matrix:
@@ -51,7 +51,7 @@ class MatrixTest(unittest.TestCase):
     # 5 6 8 8
     # 9 8 7 6
     def test_extract_column__non_square_matrix(self):
-        self.assertEqual([4, 8, 6], self.matrix_3.column(3))
+        self.assertEqual([4, 8, 6], self.matrix_3.column(4))
 
     # Test can extract column when numbers have different number of digits
     # Example matrix:
@@ -60,4 +60,4 @@ class MatrixTest(unittest.TestCase):
     # 18 3 1
     # 9 4 800
     def test_extract_column__irregular_matrix(self):
-        self.assertEqual([1903, 3, 4], self.matrix_4)
+        self.assertEqual([1903, 3, 4], self.matrix_4.column(2))
